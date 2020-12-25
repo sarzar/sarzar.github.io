6241d42e6f62aa301a905226ef3e1e39ad06b87c
EC2 Default User <ec2-user@ip-172-31-6-9.us-east-2.compute.internal>
2020-12-25 07:04:02 +0000

    css update

--- main.js
+++ main.js
@@ -3,8 +3,12 @@ $(document).ready(function(){
     console.log('Hello world');
 
     $( "#projects-link" ).click(function() {
-        $('html, body').animate({scrollTop: $('#projects').offset().top},
+    $('html, body').animate({scrollTop: $('#projects').offset().top},
             1000);
+    });
+    
+     $( "#profile-picture" ).click(function() {
+        $("profile-picture").attr("src", "https://designwoop.com/uploads/2014/01/14-2.png");
     });
 
 });
